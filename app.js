f6e5e201f82d2d503782fab7e855cf048735bbf3
airamdev11 <airamdev11@outlook.com>
2023-01-19 01:23:14 -0600

    Online

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.post("/failure", function (req, res) {
 
 
 //dynamic port
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server running on port 3000.");
 });
 
